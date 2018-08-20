0e9eb037fbbaa66c66a2f403f38b69cb9497edae
Yilin Yang <yiliny@umich.edu>
2018-08-19 20:52:39 -0400

    Add updated robot_localization rebroadcast node

---
+++ src/rebroadcast_node.cpp
@@ -0,0 +1,190 @@
+#include <ros/ros.h>
+#include "um_ardrone/navdata_altitude_rebroadcaster.h"
+#include "um_ardrone/imu_rebroadcaster.h"
+#include "um_ardrone/odometry_rebroadcaster.h"
+  using um_ardrone::ImuRebroadcaster;
+  using um_ardrone::NavdataAltitudeRebroadcaster;
+  using um_ardrone::OdometryRebroadcaster;
+  using um_ardrone::Rebroadcaster;
+
+#include <array>
+  using std::array;
+#include <memory>
+  using std::make_unique;
+  using std::unique_ptr;
+#include <stdexcept>
+  using std::runtime_error;
+#include <string>
+  using std::string;
+#include <vector>
+  using std::vector;
+
+struct RebroadcasterParams
+{
+  explicit RebroadcasterParams(const string& name_in)
+  : name{name_in}
+  {}
+
+  string paramEnable()       const { return string{"enable_"} + name;             }
+  string paramSubTopic()     const { return name + string{"_sub_topic"};          }
+  string paramPubTopic()     const { return name + string{"_pub_topic"};          }
+  string paramTfFrame()      const { return name + string{"_tf_frame"};           }
+  string paramSubQueueSize() const { return name + string{"_sub_queue_size"};     }
+  string paramPubQueueSize() const { return name + string{"_pub_queue_size"};     }
+
+  // standardized parameter names that don't have associated member vars
+  string paramChildTfFrame() const { return name + string{"_child_tf_frame"};     }
+  string paramEulerCovar()   const { return name + string{"_euler_covariance"};   }
+  string paramAngVelCovar()  const { return name + string{"_ang_vel_covariance"}; }
+  string paramLinAccCovar()  const { return name + string{"_lin_acc_covariance"}; }
+  string paramPoseCovar()    const { return name + string{"_pose_covariance"};    }
+  string paramTwistCovar()   const { return name + string{"_twist_covariance"};   }
+
+  string name;
+
+  bool should_rebroadcast;
+  string sub_topic;
+  string pub_topic;
+  string tf_frame;
+  int max_sub_queue_size;
+  int max_pub_queue_size;
+}; // struct RebroadcasterParams
+
+unique_ptr<Rebroadcaster> makeImuRebroadcaster(
+  const RebroadcasterParams& params,
+  ros::NodeHandle& node_handle
+)
+{
+  vector<double> euler_covar, ang_vel_covar, lin_acc_covar;
+  node_handle.getParam(params.paramEulerCovar(),  euler_covar);
+  node_handle.getParam(params.paramAngVelCovar(), ang_vel_covar);
+  node_handle.getParam(params.paramLinAccCovar(), lin_acc_covar);
+
+  static constexpr size_t COVAR_MAT_SIZE = 9;
+  if      (euler_covar.size() != COVAR_MAT_SIZE
+      or ang_vel_covar.size() != COVAR_MAT_SIZE
+      or lin_acc_covar.size() != COVAR_MAT_SIZE)
+  {
+    throw runtime_error{
+      "One or more badly sized IMU covariance matrices! "
+      "(All should have 9 elements!)"
+    };
+  }
+
+  array<double, COVAR_MAT_SIZE> euler_covar_arr,
+                                ang_vel_covar_arr,
+                                lin_acc_covar_arr;
+
+  static constexpr size_t NUM_MATRIX_CHARS = sizeof(double) * COVAR_MAT_SIZE;
+  memcpy(euler_covar_arr.data(),   euler_covar.data(),   NUM_MATRIX_CHARS);
+  memcpy(ang_vel_covar_arr.data(), ang_vel_covar.data(), NUM_MATRIX_CHARS);
+  memcpy(lin_acc_covar_arr.data(), lin_acc_covar.data(), NUM_MATRIX_CHARS);
+
+  return make_unique<ImuRebroadcaster>(
+    params.sub_topic,
+    params.pub_topic,
+    params.tf_frame,
+    euler_covar_arr,
+    ang_vel_covar_arr,
+    lin_acc_covar_arr
+  );
+}
+
+unique_ptr<Rebroadcaster> makeOdometryRebroadcaster(
+  const RebroadcasterParams& params,
+  ros::NodeHandle& node_handle
+)
+{
+  vector<double> pose_covar, twist_covar;
+  node_handle.getParam(params.paramPoseCovar(),  pose_covar);
+  node_handle.getParam(params.paramTwistCovar(), twist_covar);
+
+  string child_tf_frame_id;
+  node_handle.getParam(params.paramChildTfFrame(), child_tf_frame_id);
+
+  static constexpr size_t COVAR_MAT_SIZE = 36;
+  if     (pose_covar.size() != COVAR_MAT_SIZE
+      or twist_covar.size() != COVAR_MAT_SIZE)
+  {
+    throw runtime_error{
+      "One or more badly sized odometry covariance matrices! "
+      "(All should have 36 elements!)"
+    };
+  }
+
+  array<double, COVAR_MAT_SIZE> pose_covar_arr, twist_covar_arr;
+
+  static constexpr size_t NUM_MATRIX_CHARS = sizeof(double) * COVAR_MAT_SIZE;
+
+  memcpy(pose_covar_arr.data(),  pose_covar.data(),  NUM_MATRIX_CHARS);
+  memcpy(twist_covar_arr.data(), twist_covar.data(), NUM_MATRIX_CHARS);
+
+  return make_unique<OdometryRebroadcaster>(
+    params.sub_topic,
+    params.pub_topic,
+    params.tf_frame,
+    child_tf_frame_id,
+    pose_covar_arr,
+    twist_covar_arr,
+    params.max_sub_queue_size,
+    params.max_pub_queue_size
+  );
+}
+
+unique_ptr<Rebroadcaster> makeSonarRebroadcaster(
+  const RebroadcasterParams& params,
+  ros::NodeHandle& node_handle
+)
+{
+  vector<double> pose_covar;
+  node_handle.getParam(params.paramPoseCovar(),  pose_covar);
+
+  static constexpr size_t COVAR_MAT_SIZE = 36;
+  if (pose_covar.size() != COVAR_MAT_SIZE)
+  {
+    throw runtime_error{
+      string{"Badly sized SONAR altimeter covariance matrice! Should have "}
+      + std::to_string(COVAR_MAT_SIZE) + string{" elements, but instead has "}
+      + std::to_string(pose_covar.size())
+    };
+  }
+
+  array<double, COVAR_MAT_SIZE> pose_covar_arr;
+  static constexpr size_t NUM_MATRIX_CHARS = sizeof(double) * COVAR_MAT_SIZE;
+  memcpy(pose_covar_arr.data(), pose_covar.data(), NUM_MATRIX_CHARS);
+
+  return make_unique<NavdataAltitudeRebroadcaster>(
+    params.sub_topic,
+    params.pub_topic,
+    params.tf_frame,
+    pose_covar_arr,
+    params.max_sub_queue_size,
+    params.max_pub_queue_size
+  );
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "um_rebroadcast");
+  ros::NodeHandle node_handle{"~"};
+
+  vector<RebroadcasterParams> rebroadcasters{
+    RebroadcasterParams("imu"),
+    RebroadcasterParams("odometry"),
+    RebroadcasterParams("sonar"),
+  };
+
+  for (auto& r : rebroadcasters)
+  {
+    node_handle.getParam(r.paramEnable(), r.should_rebroadcast);
+    if (not r.should_rebroadcast) continue;
+    node_handle.getParam(r.paramSubTopic(),     r.sub_topic);
+    node_handle.getParam(r.paramPubTopic(),     r.pub_topic);
+    node_handle.getParam(r.paramTfFrame(),      r.tf_frame);
+    node_handle.getParam(r.paramSubQueueSize(), r.max_sub_queue_size);
+    node_handle.getParam(r.paramPubQueueSize(), r.max_pub_queue_size);
+  }
+
+  ROS_INFO("Initializing um_rebroadcast node.");
+  ROS_INFO("Loading parameters...");
+}
--- src/um_ardrone_node.cpp
+++ src/um_ardrone_node.cpp
@@ -1,43 +1,72 @@
 #include <ros/ros.h>
-#include "um_ardrone/ardrone_to_robot_localization.h"
-  using um_ardrone::ARDroneToRobotLocalization;
 
 #include <string>
   using std::string;
+
+/* #include <iostream> */
+/*   using std::cerr; */
+/*   using std::endl; */
+
+/* void receiveMessage(const nav_msgs::Odometry::ConstPtr& msg) */
+/* { */
+/*   const auto& position = msg->pose.pose.position; */
+/*   const auto& lin_vel = msg->twist.twist.linear; */
+/*   const auto& ang_vel = msg->twist.twist.angular; */
+
+/*   cerr << "========================================" */
+/*        << "\nx:\t"      << position.x */
+/*        << "\ny:\t"    << position.y */
+/*        << "\nz:\t"    << position.z */
+/*        << "\ndx:\t"   << lin_vel.x */
+/*        << "\ndy:\t"   << lin_vel.y */
+/*        << "\ndz:\t"   << lin_vel.z */
+/*        << "\ndr:\t"   << ang_vel.x */
+/*        << "\ndp:\t"   << ang_vel.y */
+/*        << "\ndyaw:\t" << ang_vel.z << endl; */
+/* } */
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "um_ardrone");
   ros::NodeHandle node_handle{"~"};
 
-  ROS_INFO("Initializing um_ardrone node.");
-  ROS_INFO("Loading parameters...");
+  /* auto sub = node_handle.subscribe("/ardrone/odometry", 100, &receiveMessage); */
 
-  bool broadcast_mag      = true;
-  bool broadcast_altitude = true;
-  bool broadcast_tum_ekf  = true;
+  /* ROS_INFO("Initializing um_ardrone node."); */
+  /* ROS_INFO("Loading parameters..."); */
 
-  string map_tf_frame_id       = "um_map";
-  string odom_tf_frame_id      = "um_odom";
-  string base_link_tf_frame_id = "um_base_link";
+  /* bool broadcast_mag      = true; */
+  /* bool broadcast_altitude = true; */
+  /* bool broadcast_tum_ekf  = true; */
 
-  node_handle.getParam("broadcast_mag",      broadcast_mag);
-  node_handle.getParam("broadcast_altitude", broadcast_altitude);
-  node_handle.getParam("broadcast_tum_ekf",  broadcast_tum_ekf);
+  /* string map_tf_frame_id       = "um_map"; */
+  /* string odom_tf_frame_id      = "um_odom"; */
+  /* string base_link_tf_frame_id = "um_base_link"; */
 
-  node_handle.getParam("map_tf_frame_id",       map_tf_frame_id);
-  node_handle.getParam("odom_tf_frame_id",      odom_tf_frame_id);
-  node_handle.getParam("base_link_tf_frame_id", base_link_tf_frame_id);
+  /* node_handle.getParam("broadcast_mag",      broadcast_mag); */
+  /* node_handle.getParam("broadcast_altitude", broadcast_altitude); */
+  /* node_handle.getParam("broadcast_tum_ekf",  broadcast_tum_ekf); */
 
-  // rebroadcast all AR.Drone messages
-  ARDroneToRobotLocalization rebroadcaster{
-    map_tf_frame_id,
-    odom_tf_frame_id,
-    base_link_tf_frame_id,
-    broadcast_altitude,
-    broadcast_mag,
-    broadcast_tum_ekf
-  };
+  /* node_handle.getParam("map_tf_frame_id",       map_tf_frame_id); */
+  /* node_handle.getParam("odom_tf_frame_id",      odom_tf_frame_id); */
+  /* node_handle.getParam("base_link_tf_frame_id", base_link_tf_frame_id); */
+
+  /* // rebroadcast all AR.Drone messages */
+  /* ARDroneToRobotLocalization rebroadcaster{ */
+  /*   map_tf_frame_id, */
+  /*   odom_tf_frame_id, */
+  /*   base_link_tf_frame_id, */
+  /*   broadcast_altitude, */
+  /*   broadcast_mag, */
+  /*   broadcast_tum_ekf */
+  /* }; */
+  /* um_ardrone::NavdataAltitudeRebroadcaster rebroadcaster{ */
+  /*   "ardrone/navdata", */
+  /*   "ardrone/altitude", */
+  /*   "um_base_link", */
+  /*   100, */
+  /*   100 */
+  /* }; */
 
   // if i
   ros::spin(); // for you like your favorite records used to,
